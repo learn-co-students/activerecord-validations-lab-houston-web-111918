170af1931dd16a7a9f79752f0a9df0219976c626
github username <github email address>
2018-12-20 14:50:09 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :is_clickbait
+  
+     def is_clickbait
+      baito = ['Won\'t Believe', 'Secret', 'Top', 'Guess']
+      testo = baito.any? {|bait| title.include?(bait) if title}
+      unless testo 
+        errors.add(:bait_status, 'Doesn\'t include bait')
+      end
+    end
 end
